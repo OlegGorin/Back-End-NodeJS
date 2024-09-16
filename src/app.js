7e24af1a42fbd103b4cebad5cc43385f4c1ff8fd
OlegGorin <olgorin@rambler.ru>
2024-09-16 17:21:48 +0200

    commit changes

--- src/app.js
+++ src/app.js
@@ -13,20 +13,20 @@ const server = http.createServer((request, response) => {
     if (userName) {
       response.statusCode = 200;
       response.statusMessage = "OK";
-      response.header = "Content-Type: text/plain, application/json";
+      response.setHeader("Content-Type", "text/plain", "application/json");
       response.end(`Hello, ${userName}`);
     } else {
       response.statusCode = 400;
-      response.header = "Content-Type: text/plain";
+      response.setHeader("Content-Type", "text/plain");
       response.end("Enter a name");
     }
     return;
   }
 
-  if (request.url === "/?users") {
+  if (searchValue.has("users")) {
     response.statusCode = 200;
     response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
+    response.setHeader("Content-Type", "application/json");
     response.write(getUsers());
     response.end();
     return;
@@ -35,11 +35,11 @@ const server = http.createServer((request, response) => {
   if (request.url === "/") {
     response.statusCode = 200;
     response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain";
-    response.end("Hello, world");
+    response.setHeader("Content-Type", "text/plain");
+    response.end("Hello, world!");
   } else {
     response.statusCode = 500;
-    response.header = "Content-Type: text/plain";
+    response.setHeader("Content-Type", "text/plain");
     response.end("");
   }
   return;
